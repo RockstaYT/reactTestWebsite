8a707a4d616d7c7199b3193b1bab33657b6aa65e
RockstaYT <joel-robles-gasser@hotmail.ch>
2019-08-11 22:16:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// components
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
+
+// includes
+import './Assets/css/default.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header />
+
+      <Homepage />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+        <div className="logo">
+            LOGO
+        </div>
+
+        <nav>
+            <ul>
+                <li className="first">
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li className="last">
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+  );
+}
+
+export default Header;
